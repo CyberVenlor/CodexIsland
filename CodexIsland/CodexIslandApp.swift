2501d1c00c9068cd7ad19006b2b254ee5bda8ad2
CyberVenlor <badcat17@163.com>
2026-04-04 19:20:43 +0800

    Initial Commit

---
+++ CodexIsland/CodexIslandApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodexIslandApp.swift
+//  CodexIsland
+//
+//  Created by n3ur0 on 4/4/26.
+//
+
+import SwiftUI
+
+@main
+struct CodexIslandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CodexIslandTests/CodexIslandTests.swift
@@ -0,0 +1,17 @@
+//
+//  CodexIslandTests.swift
+//  CodexIslandTests
+//
+//  Created by n3ur0 on 4/4/26.
+//
+
+import Testing
+@testable import CodexIsland
+
+struct CodexIslandTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
